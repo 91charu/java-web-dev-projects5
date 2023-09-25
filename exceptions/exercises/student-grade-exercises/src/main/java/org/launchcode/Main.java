bade9d2d22610868ac1c6172c68d092a005f4a88
Charu <charu91.sathish@gmail.com>
2023-09-25 14:20:08 -0500

    passing exceptions

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,10 +1,17 @@
 package org.launchcode;
-
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
+
     public static void main(String[] args) {
-        // Test out your Divide() function!
+        System.out.println("Result of division: " + Divide(10, 2));
+
+        try {
+            System.out.println("Result of division: " + Divide(10, 0));
+        } catch (ArithmeticException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -12,16 +19,35 @@ public class Main {
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
-        // Test out your CheckFileExtension() function!
+        for (Map.Entry<String, String> entry : studentFiles.entrySet()) {
+            System.out.println("Student: " + entry.getKey() + ", Points: " + CheckFileExtension(entry.getValue()));
+        }
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static int Divide(int x, int y) {
+        try {
+            if (y == 0) {
+                throw new ArithmeticException("Cannot divide by zero!");
+            }
+            return x / y;
+        } catch (ArithmeticException e) {
+            throw e;
+        }
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+    public static int CheckFileExtension(String fileName) {
+        try {
+            if (fileName == null || fileName.isEmpty()) {
+                throw new IllegalArgumentException("File name is null or empty.");
+            }
+
+            if (fileName.endsWith(".java")) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } catch (IllegalArgumentException e) {
+            throw e;
+        }
     }
 }
